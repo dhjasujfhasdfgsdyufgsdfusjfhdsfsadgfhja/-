34872b3b0e6e7caa308858d47b070814def758af
dhjasujfhasdfgsdyufgsdfusjfhdsfsadgfhja <mateionicasromania@gmail.com>
2025-04-28 00:09:43 +0100

    Update -.lua

--- -.lua
+++ -.lua
@@ -1,4 +1,4 @@
---@ kiwi_api.lua - V1.0.3
+--@ kiwi_api.lua - V1.0.4
 
 -- Place Check
 if game.PlaceId ~= 70876832253163 then
@@ -9,7 +9,7 @@ repeat
 	task.wait()
 until game:IsLoaded()
 
-warn("kiwi_api.lua - V1.0.3")
+warn("kiwi_api.lua - V1.0.4")
 
 -- Services
 Players = game:GetService("Players")
@@ -71,34 +71,36 @@ end
 -- Important Functions
 KiwiAPI.MakeSellable = function(object: Model, amount: number)
 	local function HandleSell(part: BasePart)
-		part.Touched:Connect(function(hit: BasePart)
-			if hit and hit.Name == "SellZone" and object and object:IsDescendantOf(workspace) then
-				object.Parent = nil
+		if part and part:IsA("BasePart") then
+			part.Touched:Connect(function(hit: BasePart)
+				if hit and hit.Name == "SellZone" and object and object:IsDescendantOf(workspace) then
+					object.Parent = nil
 
-				ReplicatedStorage.StopDrag:Fire()
+					ReplicatedStorage.StopDrag:Fire()
 
-				local Money = KiwiAPI.GetMoney()
+					local Money = KiwiAPI.GetMoney()
 
-				local Money_Bag: Model = game:GetObjects(Money_Bag_ID)[1]
-				Money_Bag.Parent = workspace.RuntimeItems
-				Money_Bag.MoneyBag.CFrame = hit.CFrame * CFrame.Angles(0, math.rad(90), 0) + Vector3.new(0, 3, 0)
-				Money_Bag.MoneyBag.BillboardGui.TextLabel.Text = `${amount}`
-				Money_Bag.MoneyBag.CollectPrompt.Triggered:Connect(function()
-					if not Money_Bag:GetAttribute("Collected") then
-						Money_Bag:SetAttribute("Collected", true)
+					local Money_Bag: Model = game:GetObjects(Money_Bag_ID)[1]
+					Money_Bag.Parent = workspace.RuntimeItems
+					Money_Bag.MoneyBag.CFrame = hit.CFrame * CFrame.Angles(0, math.rad(90), 0) + Vector3.new(0, 3, 0)
+					Money_Bag.MoneyBag.BillboardGui.TextLabel.Text = `${amount}`
+					Money_Bag.MoneyBag.CollectPrompt.Triggered:Connect(function()
+						if not Money_Bag:GetAttribute("Collected") then
+							Money_Bag:SetAttribute("Collected", true)
 
-						Money_Bag.Parent = nil
-						Money_Bag.MoneyBag.Collect:Play()
+							Money_Bag.Parent = nil
+							Money_Bag.MoneyBag.Collect:Play()
 
-						KiwiAPI.MoneyUpdating = true
-						Money.Value += amount
-						task.wait(0.1)
-						KiwiAPI.FakeMoney += amount
-						KiwiAPI.MoneyUpdating = false
-					end
-				end)
-			end
-		end)
+							KiwiAPI.MoneyUpdating = true
+							Money.Value += amount
+							task.wait(0.1)
+							KiwiAPI.FakeMoney += amount
+							KiwiAPI.MoneyUpdating = false
+						end
+					end)
+				end
+			end)
+		end
 	end
 
 	KiwiAPI.HandleLeaderstatsUpdates()
@@ -169,6 +171,7 @@ KiwiAPI.MakeCrafting = function(data)
 							if TrueGrids == RequiredCount then
 								for _, data in pairs(Touching) do
 									if data.Obj then
+										data.Status = false
 										data.Obj:Destroy()
 									end
 								end
@@ -184,6 +187,14 @@ KiwiAPI.MakeCrafting = function(data)
 										color.Color = data.color
 										color.Material = data.material
 										color.Transparency = data.transparency
+									end
+								end
+								
+								for _, grid: Part in Table.Grid:GetChildren() do
+									for _, touchingPart in pairs(workspace:GetPartsInPart(grid)) do
+										if touchingPart.Parent and touchingPart.Parent:GetAttribute("Set") then
+											touchingPart.Parent:SetAttribute("Set", nil)
+										end
 									end
 								end
 								
@@ -210,8 +221,177 @@ KiwiAPI.MakeCrafting = function(data)
 	end
 end
 
+KiwiAPI.MakePickable = function(tool: Model, name: string, size_multiplier: number)
+	tool:AddTag("KiwiPickable")
+	tool:SetAttribute("Name", name)
+	tool:SetAttribute("Size", size_multiplier)
+end
+
 -- Initialize
 _G.KiwiAPI = KiwiAPI
+
+-- Pickup System
+task.spawn(function()
+	ReplicatedStorage:WaitForChild("Client"):WaitForChild("Handlers"):WaitForChild("DraggableItemHandlers"):WaitForChild("ClientToolObjectHandler"):Destroy()
+	
+	local l_LocalPlayer_0 = game:GetService("Players").LocalPlayer
+	local l_HoveringObject_0 = ReplicatedStorage.Client.Handlers.DraggableItemHandlers.ClientDraggableObjectHandler.HoveringObject
+	local v3 = require(ReplicatedStorage.Shared.Utils.DraggableObjectUtil)
+	local l_isValidDraggableObject_0 = v3.isValidDraggableObject
+	local l_isDraggableObjectWelded_0 = v3.isDraggableObjectWelded
+	local v6 = require(ReplicatedStorage.Client.Controllers.ActionController)
+	local v7 = require(ReplicatedStorage.Client.DataBanks.ActionData)
+	local l_PickUpTool_0 = ReplicatedStorage.Remotes.Tool.PickUpTool
+	local l_DropTool_0 = ReplicatedStorage.Remotes.Tool.DropTool
+	local v10 = nil
+	local function v14(v11) --[[ Line: 27 ]] --[[ Name: getCurrentlyHeldTool ]]
+		if not v11 then
+			return nil
+		else
+			for _, v13 in v11:GetChildren() do
+				if v13:IsA("Tool") and v13:HasTag("Droppable") then
+					return v13
+				end
+			end
+			return nil
+		end
+	end
+	local function v17(_, v16) --[[ Line: 41 ]] --[[ Name: pickObjectActionCallback ]]
+		-- upvalues: v10 (ref), l_PickUpTool_0 (copy)
+		if v16 ~= Enum.UserInputState.Begin then
+			return Enum.ContextActionResult.Pass
+		else
+			if v10 then
+				if v10:HasTag("KiwiPickable") then
+					local RandomNum = math.random(1, 100000000)
+					
+					local Tool = Instance.new("Tool", l_LocalPlayer_0.Backpack)
+					Tool.Name = v10:GetAttribute("Name")
+					Tool.CanBeDropped = false
+					Tool:AddTag("Droppable")
+					Tool:AddTag("KiwiPickable")
+					Tool:SetAttribute("Random", RandomNum)
+					
+					local Handle = v10.PrimaryPart:Clone()
+					Handle.Name = "Handle"
+					Handle.Size *= v10:GetAttribute("Size")
+					Handle.Parent = Tool
+
+					v10.Parent = ReplicatedStorage
+					v10:SetAttribute("OGName", v10.Name)
+					
+					v10.Name = v10:GetAttribute("Name") .. RandomNum
+
+					return
+				end
+				l_PickUpTool_0:FireServer(v10)
+			end
+			return Enum.ContextActionResult.Sink
+		end
+	end
+	local function v21(_, v19) --[[ Line: 53 ]] --[[ Name: dropObjectActionCallback ]]
+		-- upvalues: v14 (copy), l_LocalPlayer_0 (copy), l_DropTool_0 (copy)
+		if v19 ~= Enum.UserInputState.Begin then
+			return Enum.ContextActionResult.Pass
+		else
+			local v20 = v14(l_LocalPlayer_0.Character)
+			if v20 then
+				if v20:HasTag("KiwiPickable") then
+					local RandomAttribute = v20:GetAttribute("Random")
+					
+					if RandomAttribute then
+						local Model = ReplicatedStorage:FindFirstChild(v20.Name .. RandomAttribute)
+						if Model then
+							local forwardVector = Character.HumanoidRootPart.CFrame.LookVector
+							local spawnPosition = Character.HumanoidRootPart.Position + (forwardVector * 7)
+							
+							Model.Name = Model:GetAttribute("OGName")
+							Model.Parent = workspace.RuntimeItems
+							Model.PrimaryPart.CFrame = CFrame.new(spawnPosition)
+						end
+					end
+					
+					v20:Destroy()
+					
+					return
+				end
+				l_DropTool_0:FireServer(v20)
+			end
+			return Enum.ContextActionResult.Sink
+		end
+	end
+	local function v24(v22) --[[ Line: 66 ]] --[[ Name: updatePickBound ]]
+		-- upvalues: v6 (copy), v7 (copy), v17 (copy)
+		local v23 = v6.isBound(v7.Action.PickUpObject)
+		if v22 and not v23 then
+			v6.bindAction(v7.Action.PickUpObject, v17, v7.ActionContext[v7.Action.PickUpObject], Enum.KeyCode.E, Enum.KeyCode.DPadLeft, v7.ActionPriority.Low)
+			return
+		else
+			if not v22 and v23 then
+				v6.unbindAction(v7.Action.PickUpObject)
+			end
+			return
+		end
+	end
+	local function v27(v25) --[[ Line: 83 ]] --[[ Name: updateDropBound ]]
+		-- upvalues: v6 (copy), v7 (copy), v21 (copy)
+		local v26 = v6.isBound(v7.Action.DropObject)
+		if v25 and not v26 then
+			v6.bindAction(v7.Action.DropObject, v21, v7.ActionContext[v7.Action.DropObject], Enum.KeyCode.Backspace, Enum.KeyCode.DPadLeft, v7.ActionPriority.Low)
+			return
+		else
+			if not v25 and v26 then
+				v6.unbindAction(v7.Action.DropObject)
+			end
+			return
+		end
+	end
+	local function v31() --[[ Line: 100 ]] --[[ Name: update ]]
+		-- upvalues: l_LocalPlayer_0 (copy), v24 (copy), v27 (copy), l_HoveringObject_0 (copy), v14 (copy), l_isValidDraggableObject_0 (copy), l_isDraggableObjectWelded_0 (copy), v10 (ref)
+		local l_Character_0 = l_LocalPlayer_0.Character
+		if not l_Character_0 then
+			v24(false)
+			v27(false)
+			return
+		else
+			local l_Value_0 = l_HoveringObject_0.Value
+			local v30 = v14(l_Character_0)
+			if l_Value_0 and l_isValidDraggableObject_0(l_Value_0) and not l_isDraggableObjectWelded_0(l_Value_0) and l_Value_0:HasTag("ToolObject") and (not l_Value_0:GetAttribute("OwnerId") or l_Value_0:GetAttribute("OwnerId") == l_LocalPlayer_0.UserId) then
+				v10 = l_Value_0
+				if l_Value_0:HasTag("ShopItem") then
+					v10 = nil
+				end
+			else
+				v10 = nil
+			end
+			v24(v10 ~= nil)
+			v27(v30 ~= nil)
+			return
+		end
+	end
+	local function v36(v32) --[[ Line: 130 ]] --[[ Name: onCharacterAdded ]]
+		-- upvalues: v31 (copy), v24 (copy), v27 (copy)
+		local function v33() --[[ Line: 131 ]] --[[ Name: handleChildChanged ]]
+			-- upvalues: v31 (ref)
+			v31()
+		end
+		local v34 = v32.ChildAdded:Connect(v33)
+		local v35 = v32.ChildRemoved:Connect(v33)
+		v32.Destroying:Once(function() --[[ Line: 138 ]]
+			-- upvalues: v34 (copy), v35 (copy), v24 (ref), v27 (ref)
+			v34:Disconnect()
+			v35:Disconnect()
+			v24(false)
+			v27(false)
+		end)
+		v31()
+	end
+	if l_LocalPlayer_0.Character then
+		v36(l_LocalPlayer_0.Character)
+	end
+	l_HoveringObject_0.Changed:Connect(v31)
+	l_LocalPlayer_0.CharacterAdded:Connect(v36)
+end)
 
 -- Dragging System
 ReplicatedStorage:WaitForChild("Client"):WaitForChild("Handlers"):WaitForChild("DraggableItemHandlers"):WaitForChild("ClientDraggableObjectHandler").Enabled = false
